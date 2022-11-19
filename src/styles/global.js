5d7c4037dc6a722510ecd78e4454d6ed4a3885f3
여다희 <yeodahui.dev@gmail.com>
2022-11-19 16:09:22 +0900

    스타일: 테마 컬러 수정 및 전역 스타일 수정

--- src/styles/global.js
+++ src/styles/global.js
@@ -2,10 +2,26 @@ import { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
 
 export const GlobalStyles = createGlobalStyle`
-${reset}
+    ${reset}
 
-  html, body, #root, .App {
-    width: 100%;
-    height: 100%;
-  }
+    * { box-sizing: border-box }
+
+    .a11y-hidden {
+        overflow: hidden;
+        position: absolute;
+        clip: rect(0, 0, 0, 0);
+        width: 1px;
+        height: 1px;
+        margin: -1px;
+        border: 0;
+        padding: 0;
+    }
+
+    html, body, #root, .App {
+        width: 100%;
+        height: 100%;
+        background-color: ${({ theme }) => theme.colors.background};
+        font-size: 10px;
+        font-family: ${({ theme }) => theme.font};
+    }
 `;
